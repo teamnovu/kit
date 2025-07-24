7f397ccc0f34e7389e26e025459101cddb0c52bc
Felix Rauchenstein <felix.rauchenstein@gmx.ch>
2025-07-24 16:05:27 +0200

    rearrange

--- packages/vue-forms/src/components/Field.vue
+++ packages/vue-forms/src/components/Field.vue
@@ -4,17 +4,17 @@
 
 <script
   setup lang="ts"
-  generic="TPath extends Paths<TData>, TData extends object"
+  generic="TData extends object, TPath extends Paths<TData>"
 >
 import type { Form } from '../types/form.ts'
 import type { Paths, PickProps } from '../types/util.ts'
 import type { UseFieldOptions } from '../composables/useField.ts'
 
-export interface FieldProps<TPath extends string, TData extends object> extends UseFieldOptions<PickProps<TData, TPath>, TPath> {
+export interface FieldProps<TData extends object, TPath extends string> extends UseFieldOptions<PickProps<TData, TPath>, TPath> {
   form: Form<TData>
 }
 
-const props = defineProps<FieldProps<TPath, TData>>()
+const props = defineProps<FieldProps<TData, TPath>>()
 
 const field = props.form.defineField({
   path: props.path,
