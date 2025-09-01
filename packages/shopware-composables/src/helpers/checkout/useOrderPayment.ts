ba3ef7aef96cdc9f790cc7e45330883dde613426
Elias Bernhaut <elias.bernhaut@novu.ch>
2025-09-01 09:52:09 +0200

    fix(shopware-composables): remove 'isAsynchronous' as it is removed in 6.7

--- packages/shopware-composables/src/helpers/checkout/useOrderPayment.ts
+++ packages/shopware-composables/src/helpers/checkout/useOrderPayment.ts
@@ -2,7 +2,6 @@ import type { MaybeRef } from 'vue'
 import { computed, unref } from 'vue'
 import type { Schemas } from '../../query/types/operations'
 import type { BrandedSchema } from '../types/schema'
-import { isAsynchronousOrder } from '../../util'
 
 export function useOrderPayment(
   order: MaybeRef<Schemas['Order'] | null | undefined>,
@@ -13,10 +12,7 @@ export function useOrderPayment(
   const paymentMethod = computed(() => activeTransaction.value?.paymentMethod as BrandedSchema<'PaymentMethod'> | undefined)
   const state = computed(() => activeTransaction.value?.stateMachineState as BrandedSchema<'StateMachineState'> | undefined)
 
-  const isAsynchronous = computed(() => isAsynchronousOrder(order))
-
   return {
-    isAsynchronous,
     activeTransaction,
     state,
     paymentMethod,
