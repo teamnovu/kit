91e66658a7b9b75eff325c243b2dd9dc80b165ca
Elias Bernhaut <elias.bernhaut@novu.ch>
2025-10-07 09:20:15 +0200

    fix: field reactivity

--- packages/vue-forms/src/components/Field.vue
+++ packages/vue-forms/src/components/Field.vue
@@ -1,12 +1,11 @@
 <template>
-  <slot v-bind="slotData" />
+  <slot v-bind="field" />
 </template>
 
 <script
   setup lang="ts"
   generic="TData extends object, TPath extends Paths<TData>"
 >
-import { reactive } from 'vue'
 import type { Form } from '../types/form.ts'
 import type { Paths, PickProps } from '../types/util.ts'
 import type { UseFieldOptions } from '../composables/useField.ts'
@@ -20,6 +19,4 @@ const props = defineProps<FieldProps<TData, TPath>>()
 const field = props.form.defineField({
   path: props.path,
 })
-
-const slotData = reactive(field)
 </script>
